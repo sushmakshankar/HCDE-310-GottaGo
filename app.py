f87e797d3c24fe73d065ff843ff9069c39083680
Madison <mmam@Madisons-MacBook-Pro-4.local>
2024-11-25 16:39:37 -0800

    added additional html files and (tried) to psuedocode them :)

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 from flask import Flask
 
 #flask development here
+
+#METHOD 2
+#index method to initalize homepage
+# @app
+
+#METHOD 2
+#method to collect locational query from user
+#@app
+#provides spaces for the user to input their search parameter data
+#location queery (text box)
+# optional language (drop down menu? )
+
+
+#possible method?
+# allow user to choose event based on query results.
+
+#METHOD 3
+#method to connect query search results from selected event
+#@app
+
+#connects secondary html page selection (METHOD 2) to avalible nearby restrooms
+#presents a list of restrooms nearby geolocational data.
+# should use python api call
+#
